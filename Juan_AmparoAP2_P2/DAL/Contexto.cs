bc8e018189d852edde54fb5e1ae1bc130ce51c89
JuanCarlos <JuanCarlos@DESKTOP-1H4K3BB>
2026-03-02 20:44:38 -0400

    preparando el contexto para el parcial

---
+++ Juan_AmparoAP2_P2/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Juan_AmparoAP2_P2.Models;
+
+namespace Juan_AmparoAP2_P2.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+    }
+
+    public DbSet<ViajesEspaciales> ViajesEspaciales { get; set; } = null!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<ViajesEspaciales>().ToTable("ViajesEspaciales");
+    }
+}
