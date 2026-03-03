63aaf31c4eb634c086c022e383bb3402765bb6b0
JuanCarlos <JuanCarlos@DESKTOP-1H4K3BB>
2026-03-02 20:44:29 -0400

    la clase de viajes espaciales

---
+++ Juan_AmparoAP2_P2/Models/ViajesEspaciales.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Juan_AmparoAP2_P2.Models;
+
+public class ViajesEspaciales
+{
+    [Key]
+    public int ViajeId { get; set; }
+
+    [Required(ErrorMessage = "El nombre del viaje es obligatorio.")]
+    public string? Nombre { get; set; }
+
+    [Required(ErrorMessage = "La fecha es obligatoria.")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Range(0.1, double.MaxValue, ErrorMessage = "El costo debe ser mayor a 0.")]
+    public decimal Costo { get; set; }
+}
